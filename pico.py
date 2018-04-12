71ac973132d7fe6225b3dd4ef077c5ea0d655cd9
Brian E Bennett <brian@ohheybrian.com>
2018-04-12 11:51:11 -0400

    Social links as YAML
    Cleaning up the config view, adding dynamic social links with YAML. A user
    can define sites to be displayed on the left-hand sidebar.
    
    Added a helper function to `pico.py` to pull navigation for all pages.

--- pico.py
+++ pico.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, jsonify, url_for
 from slugify import slugify
-import os, glob, datetime, re, string, sys
-from urllib.parse import urlparse
-from werkzeug.contrib.atom import AtomFeed
+import os, glob, re, string, sys, yaml
 
 
 app = Flask(__name__)
@@ -40,8 +38,16 @@ def check_for_title(file, slug):
             else:
                 return False
 
+def get_nav():
+    stream = open('config.yaml', 'r')
+    nav = yaml.load(stream)
+    return nav
+
 @app.route('/')
 def index():
+
+    nav = get_nav()
+    
     content = []
 
     for file in dir:
@@ -53,10 +59,11 @@ def index():
 
     sorted_content = sorted(content, key=lambda blog: blog.post_date, reverse=True)
 
-    return render_template('index.html', content=sorted_content)
+    return render_template('index.html', content=sorted_content, nav=nav['social'])
 
 @app.route('/post/<slug>')
 def single_post(slug):
+    nav = get_nav()
 
     for file in dir:
         if not file.startswith('.'):
@@ -64,7 +71,7 @@ def single_post(slug):
             with open('files/' + file, 'r') as f:
                 if check_for_title(file, slug):
                     item = process_text_file(f)
-                    return render_template('entry.html', content=item)
+                    return render_template('entry.html', content=item, nav=nav['social'])
 
 def process_text_file(item):
     txt = item.readlines()
@@ -86,23 +93,23 @@ def process_text_file(item):
 
 
 # TODO: Create ATOM feed from txt files
-@app.route('/recent.atom')
-def make_external(url):
-    return urljoin(request.url_root, url)
-
-def recent_feed():
-    feed = AtomFeed('Recent Articles',feed_url=request.url, url=request.url_root)
-    articles = glob.glob('files/*.txt')
-
-    for article in articles:
-        article = parse_txt_file(article)
-        feed.add(article.title, article.body,
-                 content_type='html',
-                 # author=article.author.name,
-                 url=make_external(article.url),
-                 # updated=article.last_update,
-                 published=article.published)
-    return feed.get_response()
+# @app.route('/recent.atom')
+# def make_external(url):
+#     return urljoin(request.url_root, url)
+#
+# def recent_feed():
+#     feed = AtomFeed('Recent Articles',feed_url=request.url, url=request.url_root)
+#     articles = glob.glob('files/*.txt')
+#
+#     for article in articles:
+#         article = parse_txt_file(article)
+#         feed.add(article.title, article.body,
+#                  content_type='html',
+#                  # author=article.author.name,
+#                  url=make_external(article.url),
+#                  # updated=article.last_update,
+#                  published=article.published)
+#     return feed.get_response()
 
 
 if __name__ == "__main__":
